8b59b8e6da8bc5e65232bc7d5d555f1a63744513
Mike-show <76592022+Mike-show@users.noreply.github.com>
2021-05-06 11:48:02 +0300

    17 solved

--- 17/17.c
+++ 17/17.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-int CountH(int H);
 int CountT(int T);
 int CountU(int U);
+#define AND 3;
+#define ONETHOUSAND 11;
+#define HUNDRED 7;
 
 int main(void){
     int Result = 0;
@@ -21,10 +23,11 @@ int main(void){
     for(int i=100; i<=999; i++){
         int Hundreds, Tens, Units, Temp;
         Hundreds = i/100;
-        Result += CountH(Hundreds); //one hundred~nine hundred
+        Result += CountU(Hundreds); //one~nine
+        Result += HUNDRED;
         Tens = i-Hundreds*100;
         if(Tens != 0){
-            Result += 3;  //and
+            Result += AND;  //and
             if(Tens >= 20){
                 Temp = Tens/10;
                 Result += CountT(Temp); // twenty~ninety
@@ -36,153 +39,118 @@ int main(void){
             }
         }
     }
-    Result += 11; //one thousand
+    Result += ONETHOUSAND; //one thousand
     printf("%d\n", Result);
 }
 
-int CountH(int H){
-    switch (H)
-    {
-    case 0:
-        return 0;
-        break;
-    case 1:
-        return 10;
-        break;
-    case 2:
-        return 10;
-        break;
-    case 3:
-        return 12;
-        break;
-    case 4:
-        return 11;
-        break;
-    case 5:
-        return 11;
-        break;
-    case 6:
-        return 10;
-        break;
-    case 7:
-        return 12;
-        break;
-    case 8:
-        return 12;
-        break;
-    case 9:
-        return 11;
-        break;
-    default:
-        printf("Error\n");
-        break;
-    }
-}
-
 int CountT(int T){
+    int R;
     switch (T)
     {
     case 0:
-        return 0;
+        R = 0;
         break;
     case 1:
-        return 0;
+        R = 0;
         break;
     case 2:
-        return 6;
+        R = 6;//twenty
         break;
     case 3:
-        return 6;
+        R = 6;//thirty
         break;
     case 4:
-        return 5;
+        R = 5;//forty
         break;
     case 5:
-        return 5;
+        R = 5;//fifty
         break;
     case 6:
-        return 5;
+        R = 5;//sixty
         break;
     case 7:
-        return 7;
+        R = 7;//seventy
         break;
     case 8:
-        return 6;
+        R = 6;//eighty
         break;
     case 9:
-        return 6;
+        R = 6;//ninety
         break;
     default:
         printf("Error\n");
         break;
     }
+    return R;
 }
 
 int CountU(int U){
+    int R;
     switch (U)
     {
     case 0:
-        return 0;
+        R = 0;
         break;
     case 1:
-        return 3;
+        R = 3;//one
         break;
     case 2:
-        return 3;
+        R = 3;//two
         break;
     case 3:
-        return 3;
+        R = 5;//three
         break;
     case 4:
-        return 4;
+        R = 4;//four
         break;
     case 5:
-        return 4;
+        R = 4;//five
         break;
     case 6:
-        return 3;
+        R = 3;//six
         break;
     case 7:
-        return 5;
+        R = 5;//seven
         break;
     case 8:
-        return 5;
+        R = 5;//eight
         break;
     case 9:
-        return 4;
+        R = 4;//nine
         break;
     case 10:
-        return 3;
+        R = 3;//ten
         break;
     case 11:
-        return 6;
+        R = 6;//eleven
         break;
     case 12:
-        return 6;
+        R = 6;//twelve
         break;
     case 13:
-        return 8;
+        R = 8;//thirteen
         break;
     case 14:
-        return 8;
+        R = 8;//fourteen
         break;
     case 15:
-        return 7;
+        R = 7;//fifteen
         break;
     case 16:
-        return 7;
+        R = 7;//sixteen
         break;
     case 17:
-        return 9;
+        R = 9;//seventeen
         break;
     case 18:
-        return 8;
+        R = 8;//eighteen
         break;
     case 19:
-        return 8;
+        R = 8;//nineteen
         break;
     default:
         printf("Error\n");
         break;
     }
-}
+    return R;
+};
