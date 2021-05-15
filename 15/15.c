6eb4d9111b09247d39477701ab8ce4099642ef40
Mike-show <76592022+Mike-show@users.noreply.github.com>
2021-05-15 22:08:11 +0300

    master

--- 15/15.c
+++ 15/15.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 int main(void){
     double Grid = 20;
-    double Step = 2 * Grid;
+    double Step = 2 * Grid; // in a matrix contains X cubes, 2*X steps are needed from one corner to the diagonal corner
     double Uper = 1, Lower = 1;
     for(double i=Step; i>Grid; i--){
         Lower = i/(i-Grid);
--- 18/18.c
+++ 18/18.c
@@ -1,22 +1,52 @@
 #include <stdio.h>
-void Read(int Array[15][30], FILE *fp);
+void Read(int Array[15][15], FILE *fp);
 
 int main(void){
-    int Array[15][30] = {0};
+    int Array[15][15] = {0};
+    int ParentRow[15] = {0};
+    int RightPath, LeftPath;
+    int MaxNum = 0; 
     FILE *fp;
     fp = fopen("data.txt", "r");
     Read(Array, fp);
+    ParentRow[0] = 75;
+    for(int i=1; i<15; i++){
+        int Temp[15] = {0};
+        for(int j=0; j<=i; j++){
+            if(j == 0){
+                Temp[j] = Array[i][j] + ParentRow[j];
+            }
+            else {
+                if(j == i){
+                    Temp[j] = Array[i][j] + ParentRow[j-1];
+                }
+                else {
+                    RightPath = Array[i][j] + ParentRow[j];
+                    LeftPath = Array[i][j] + ParentRow[j-1];
+                    if(RightPath > LeftPath) Temp[j] = RightPath;
+                    else Temp[j] = LeftPath;
+                }
+            }
+        }
+        for(int k=0; k<=i; k++){
+            ParentRow[k] = Temp[k];
+        }
+    }
     for(int i=0; i<15; i++){
-        for(int j=0; j<30; j++){
+        if(ParentRow[i] > MaxNum) MaxNum = ParentRow[i];
+    }
+    printf("%d\n", MaxNum);
+    /*for(int i=0; i<15; i++){
+        for(int j=0; j<15; j++){
             printf("%d  ", Array[i][j]);
         }
         printf("\n");
-    }
+    }*/
 }
 
-void Read(int Array[15][30], FILE *fp){
+void Read(int Array[15][15], FILE *fp){
     for(int i=0; i<15; i++){
-        for(int j=14-i; j<=14+i; j+=2){
+        for(int j=0; j<=i; j++){
             fscanf(fp, "%d", &Array[i][j]);
         }
     }
