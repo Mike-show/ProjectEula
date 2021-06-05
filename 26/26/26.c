e4823d96c8b2924b7fca279ea2a07028a2462410
Mike-show <76592022+Mike-show@users.noreply.github.com>
2021-06-05 15:30:21 +0300

    26 solved

--- 26/26/26.c
+++ 26/26/26.c
@@ -1,46 +1,63 @@
 #include <stdio.h>
 #include <math.h>
 
-#define BITSIZE 10
+#define BITSIZE 6000
 
-int Divide(int d, int Digit, int a[BITSIZE]);
-int DigitCount(int d);
+int Divide(int d, int a[BITSIZE]);
+int CircleCount(int a[BITSIZE]);
+void ArrayReset(int a[BITSIZE]);
 
 int main(int argc, const char * argv[]) {
-    int d = 73, bit = 0;
     int a[BITSIZE] = {0};
-        int Digit = 0;
-        Digit = DigitCount(d);
-        bit = Divide(d, Digit, a);
-    printf("0.");
-    for(int i=0; i<BITSIZE; i++){
-        printf("%d", a[i]);
-    }
-}
-
-int DigitCount(d){
-    int Digit = 0;
-    if(d/1000 != 0) Digit = 4;
-    else {
-        if(d/100 != 0) Digit = 3;
-        else{
-            if(d/10 != 0) Digit = 2;
-            else Digit = 1;
+    int MaxCircle = 1;
+    int MaxCircleNum = 1;
+    int CircleJudge = 0;
+    for(int i=1; i<1000; i++){
+        int Temp = 1;
+        CircleJudge = Divide(i, a);   //divde 1 by i, store the result into an array
+        if(CircleJudge == 1) {
+            Temp = CircleCount(a);
+            if(Temp > MaxCircle){
+                MaxCircle = Temp;
+                MaxCircleNum = i;
+            }
         }
+        ArrayReset(a);
     }
     
-    return Digit;
+    printf("Number %d has a max circle whose length is %d\n", MaxCircleNum, MaxCircle);
+    return 1;
 }
 
-int Divide(int d, int Digit, int a[BITSIZE]){
-    int Quotient, Remainder = 1, Numerator, bit = Digit-1;
-    int Pow = pow(10, Digit);
+int Divide(int d, int a[BITSIZE]){
+    int Quotient, Remainder = 1, Numerator = 1, bit = 0, Judge = 1;
     do{
-        Numerator = Remainder;
-        Quotient = Numerator*Pow/ d;
-        Remainder = (Numerator*Pow)%d;
+        Quotient = Numerator / d;
+        Remainder = Numerator % d;
+        Numerator = Remainder * 10;
         a[bit] = Quotient;
-        bit+=Digit;
-    }while(Remainder != 0 && bit<BITSIZE);
-    return bit;
+        bit++;
+    }while(bit<BITSIZE && Remainder != 0);
+    if(Remainder == 0) Judge = 0;
+    return Judge;
+}
+
+int CircleCount(int a[BITSIZE]){
+    int CircleDigit = 5;
+    int CircleConfirm;
+    do{
+        CircleConfirm = 1;
+        for(int i=1; i<=CircleDigit && CircleConfirm != 0; i++){
+            if(a[BITSIZE-i] != a[BITSIZE-(i+CircleDigit)] || a[BITSIZE-i] != a[BITSIZE-(i+2*CircleDigit)]) CircleConfirm = 0;
+        }
+        if(CircleConfirm == 0) CircleDigit++;
+    }while(CircleConfirm == 0 && CircleDigit <= BITSIZE/3);
+    
+    return CircleDigit;
+}
+
+void ArrayReset(int a[BITSIZE]){
+    for(int i=0; i<BITSIZE; i++){
+        a[i] = 0;
+    }
 }
